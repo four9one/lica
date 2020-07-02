b8407aac54d7d8b0ae8a5d3974f2eb6959822474
Nicklas Forsell <38962959+four9one@users.noreply.github.com>
2020-07-02 05:26:49 +0200

    Add files via upload

---
+++ build/precache-manifest.f8b6789b6132a10acec8754815485976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc9d8bb662d1e264a70ec14ad13aec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee2d2d709639276f478e",
+    "url": "/static/css/main.53b13fb8.chunk.css"
+  },
+  {
+    "revision": "c448910050a8754b5012",
+    "url": "/static/js/2.e5abeb6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5abeb6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2d2d709639276f478e",
+    "url": "/static/js/main.ef656b33.chunk.js"
+  },
+  {
+    "revision": "c4154b566f61f72e8e6c",
+    "url": "/static/js/runtime-main.b41fc40d.js"
+  }
+]);
